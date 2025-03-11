f3b0f6b29fcbfe363a8c0b49f2b09f0c278069df
enwulf <prachodayaat@yandex.ru>
2025-03-11 15:15:48 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+  id("java")
+  id("org.jetbrains.kotlin.jvm") version "1.9.25"
+  id("org.jetbrains.intellij") version "1.17.4"
+}
+
+group = "ru.enwulf"
+version = "1.0.0"
+
+repositories {
+  mavenCentral()
+}
+
+intellij {
+  version.set("2024.1.7")
+}
+
+tasks {
+  withType<JavaCompile> {
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
+  }
+
+  patchPluginXml {
+    sinceBuild.set("241")
+    untilBuild.set("243.*")
+  }
+}
